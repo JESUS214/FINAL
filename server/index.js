ce6155efa11d293411fda86ba234d87a671272c4
Jesus Flores <jesus4flores@gmail.com>
2022-08-16 16:53:17 -0700

    added mongoDB

--- server/index.js
+++ server/index.js
@@ -7,3 +7,12 @@ const app = express();
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors());
+// went to mongodb to create new cluster, get the website. connect and put in username password(in real life make sure to use secure username and password)
+const CONNECTION_URL = 'mongodb+srv://sdsufinal:mnkay214@cluster0.ax8xyov.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+// use mongoose to connect to database
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, ()=> console.log('Server running on port: ${PORT}')))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
