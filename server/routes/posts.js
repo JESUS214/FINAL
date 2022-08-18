2c7d8bb5176ce26110464d89494fe0065c98cfbc
Jesus Flores <jesus4flores@gmail.com>
2022-08-18 01:32:06 -0700

    added delete

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getPosts, createPost, updatePost} from '../controllers/posts.js';
+import {getPosts, createPost, updatePost, deletePost, likePost} from '../controllers/posts.js';
 
 const router = express.Router();
 
@@ -8,4 +8,7 @@ router.post('/', createPost);
 //patch is used for updating existing documents
 //the color in /:id is to make it dynamic
 router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
+
 export default router;
